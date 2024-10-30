ddea94d5831374a1041425e7a652d2fcba536bae
Mattia Pozzati <mattia.pozzati3@studio.unibo.it>
2024-10-30 11:46:49 +0100

    Completato esercizio 62

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,8 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +12,31 @@ import java.util.Map;
  */
 public final class UseListsAndMaps {
 
+
+    static class Range {
+
+        private int min;
+        private int max;
+        
+        Range(final int min, final int max){
+            this.min = min;
+            this.max = max;
+        }
+
+        public List<Integer> listRange(){
+            List<Integer> aList = new ArrayList<>();
+            for(int i = min; i <= max; i++){
+                aList.add(i);
+            }
+            return aList;
+        }
+        
+    }
+
+    /**
+     * InnerUseListsAndMaps
+     */
+
     private UseListsAndMaps() {
     }
 
@@ -17,23 +45,37 @@ public final class UseListsAndMaps {
      *            unused
      */
     public static void main(final String... s) {
+        var r = new Range(1000, 2000);
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
+         *
          */
+
+        List<Integer> aList = new ArrayList<Integer>(r.listRange());
+
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+
+        List<Integer> lList = new LinkedList<Integer>(r.listRange());
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        final int temp = aList.getFirst();
+        aList.set(0,aList.get(aList.size()-1));
+        aList.set(aList.size() - 1, temp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+
+        for (Integer integer : aList) {
+            System.out.println(integer);
+        }  
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +83,44 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = System.nanoTime();
+
+        for (int i = 0; i < 100.000; i++) {
+            aList.addFirst(1);
+        }
+
+        System.out.println("The function fun1 with arrayList implementation fineshes in " + (System.nanoTime() - time) + "ms");
+        
+        time = System.nanoTime();
+
+        for (int i = 0; i < 100.000; i++) {
+            lList.addFirst(1);
+        }
+
+        System.out.println("The function fun1 with linked list implementation fineshes in " + (System.nanoTime() - time) + "ms");   
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        
+        time = System.nanoTime();
+
+        for (int i = 0; i < 100.000; i++) {
+            aList.get(aList.size()/2);
+        }
+
+        System.out.println("The function fun2 with arrayList implementation fineshes in " + (System.nanoTime() - time) + "ms");
+        
+        time = System.nanoTime();
+
+        for (int i = 0; i < 100.000; i++) {
+            lList.get(aList.size()/2);
+        }
+
+        System.out.println("The function fun2 with linked list implementation fineshes in " + (System.nanoTime() - time) + "ms\n\n");   
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -66,5 +140,21 @@ public final class UseListsAndMaps {
         /*
          * 8) Compute the population of the world
          */
+
+        Map<String, Long> population = new HashMap<String, Long>();
+        population.put("Africa", 1_110_635_000L);
+        population.put("Americas", 972_005_000L);
+        population.put("Antarctica", 0L);
+        population.put("Asia", 4_298_723_000L);
+        population.put("Europe", 742_452_000L);
+        population.put("Oceania", 38_304_000L);
+
+        long total=0;
+
+        for (Map.Entry<String, Long> entry : population.entrySet()) {
+            System.out.println(entry.getKey()+": "+entry.getValue());
+            total += entry.getValue();
+        }
+        System.out.println("-------------\nTotal: " + total);
     }
 }
